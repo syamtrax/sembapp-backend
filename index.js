e61040a6634324401c96d116740f182d110e5f5e
Satria Syammahestatma <satriasyamm@gmail.com>
2022-11-15 06:38:37 +0700

    edit cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    //origin: "http://localhost:3000",
     optionsSuccessStatus: 200,
   })
 );
