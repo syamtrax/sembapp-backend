db0957a0f082d0bfd220f7cff713d3838b9ed306
Satria Syammahestatma <satriasyamm@gmail.com>
2022-11-15 08:15:51 +0700

    db

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("sembapp_db", "root", "", {
-  host: "localhost",
+const db = new Sequelize("sembapp_db", "admin", "YBjca2xl", {
+  host: "mysql-95952-0.cloudclusters.net",
   dialect: "mysql",
 });
 
