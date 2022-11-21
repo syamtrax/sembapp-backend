b314474d1db8f1bcc397afcef8c8b3414c98024d
Satria Syammahestatma <satriasyamm@gmail.com>
2022-11-21 17:26:27 +0700

    new

--- config/database.js
+++ config/database.js
@@ -14,4 +14,12 @@ const db = new Sequelize("newsembapp", "syamtrax", "Cobalogin123", {
   },
 });
 
+db.authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
+
 export default db;
