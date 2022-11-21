fa6aeb1bc450a277b202893d4521503b9323209c
Satria Syammahestatma <satriasyamm@gmail.com>
2022-11-21 09:11:11 +0700

    ++

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize("sembapp_db", "root", "", {
-  host: "localhost",
+  host: "sembapp.database.windows.net",
   dialect: "mysql",
 });
 
