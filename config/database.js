1ca1940b965d7fe8f8c1a383dab95a58e4692ac6
Satria Syammahestatma <satriasyamm@gmail.com>
2022-11-21 17:18:49 +0700

    timout

--- config/database.js
+++ config/database.js
@@ -3,7 +3,12 @@ import { Sequelize } from "sequelize";
 const db = new Sequelize("newsembapp", "syamtrax", "Cobalogin123", {
   host: "sembappdbcoba.mysql.database.azure.com",
   dialect: "mysql",
-  port : 3306,
+  port: 3306,
+  pool: {
+    max: 10,
+    min: 0,
+    idle: 10000,
+  },
 });
 
 export default db;
