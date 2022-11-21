29bc314d59c724306d5d4f24edd78ba6bfb6be17
Satria Syammahestatma <satriasyamm@gmail.com>
2022-11-21 16:44:46 +0700

    edit db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("dbs_sembapp", "syamtrax", "Cobalogin123", {
+const db = new Sequelize("newsembapp", "syamtrax", "Cobalogin123", {
   host: "sembapp.database.windows.net",
   dialect: "mysql",
 });
