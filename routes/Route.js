45731c683c4722fcc9477d1b3c0025dc52758f00
Satria Syammahestatma <satriasyamm@gmail.com>
2022-11-22 04:40:02 +0700

    v

--- routes/Route.js
+++ routes/Route.js
@@ -32,14 +32,14 @@ import {
   deleteDocument,
   getDocument,
   updateDocument,
-  getDocumentByID
+  getDocumentByID,
 } from "../controllers/documentController.js";
 
 const router = express.Router();
 
-router.get("/transaction", verifyToken, getTransaction);
-router.get("/transaction/:id", verifyToken, getTransactionByID);
-router.post("/transaction", verifyToken, createTransaction);
+router.get("/transaction", getTransaction);
+router.get("/transaction/:id", getTransactionByID);
+router.post("/transaction", createTransaction);
 router.patch("/transaction/:id", updateTransaction);
 router.delete("/transaction/:id", deleteTransaction);
 router.get("/total", totalTransaction);
@@ -49,19 +49,19 @@ router.get("/user", verifyToken, getUser);
 router.post("/user", Register);
 
 router.post("/login", Login);
-router.get("/token", refreshToken);
+router.get("/token", verifyToken, refreshToken);
 router.delete("/logout", Logout);
 
-router.get("/produk",verifyToken, getProduct);
-router.get("/produk/:kodeProduk",verifyToken, getProductByID);
-router.post("/produk",verifyToken, createProduct);
-router.patch("/produk/:kodeProduk", verifyToken,updateProduct);
-router.delete("/produk/:kodeProduk", verifyToken,deleteProduct);
+router.get("/produk", getProduct);
+router.get("/produk/:kodeProduk", getProductByID);
+router.post("/produk", createProduct);
+router.patch("/produk/:kodeProduk", updateProduct);
+router.delete("/produk/:kodeProduk", deleteProduct);
 
-router.get("/dokumen",verifyToken, getDocument);
-router.get("/dokumen/:id", verifyToken,getDocumentByID);
-router.post("/dokumen", verifyToken,createDocument);
-router.patch("/dokumen/:id",verifyToken, updateDocument);
-router.delete("/dokumen/:id",verifyToken, deleteDocument);
+router.get("/dokumen", getDocument);
+router.get("/dokumen/:id", getDocumentByID);
+router.post("/dokumen", createDocument);
+router.patch("/dokumen/:id", updateDocument);
+router.delete("/dokumen/:id", deleteDocument);
 
 export default router;
