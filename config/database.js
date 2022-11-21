d61df0e84b65232c19a27fb7da6a18fc6bf47bfa
Satria Syammahestatma <satriasyamm@gmail.com>
2022-11-21 16:56:18 +0700

    change server

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize("newsembapp", "syamtrax", "Cobalogin123", {
-  host: "sembapp.database.windows.net",
+  host: "sembappdbcoba.mysql.database.azure.com",
   dialect: "mysql",
 });
 
